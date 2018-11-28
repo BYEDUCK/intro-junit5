b2fae3d3d0dfbf1642bfac60ced785612ef5d1f1
Mateusz Bajdak <m.bajdak@AMDC2218.digital.local>
2018-11-28 16:19:52 +0100

    first

---
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -0,0 +1,27 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreetingTest {
+
+    private Greeting greeting;
+
+    @BeforeEach
+    void setUp() {
+        greeting = new Greeting();
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void helloWorld1() {
+        System.out.println(greeting.helloWorld("Mateusz"));
+    }
+}
